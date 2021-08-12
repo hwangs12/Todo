81439d07c7e7b3790809050f7c85b69d95abdb3b
hwangs12 <sungjun94@gmail.com>
2021-08-11 22:45:50 -0400

    Updates

---
+++ precache-manifest.a63b5186eded568395aa7ed8e3f30e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a13d015151f34dcb66eef90636ca4b",
+    "url": "/todo-v2/index.html"
+  },
+  {
+    "revision": "34c8e38954909e9bcd1a",
+    "url": "/todo-v2/static/css/main.cefd03e3.chunk.css"
+  },
+  {
+    "revision": "89f1e234b7795df0c029",
+    "url": "/todo-v2/static/js/2.a5359244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-v2/static/js/2.a5359244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c8e38954909e9bcd1a",
+    "url": "/todo-v2/static/js/main.e4b49b93.chunk.js"
+  },
+  {
+    "revision": "a61874125303e920de0f",
+    "url": "/todo-v2/static/js/runtime-main.edcfd5ee.js"
+  }
+]);
